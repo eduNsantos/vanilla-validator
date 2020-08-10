3d7fbc76f6456331b1fd588ca3b6b44cf79d126c
Eduardo Santos <consig5@grupoamp.com.br>
2020-08-10 14:54:00 -0300

    validating form fields

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-document.addEventListener('DOMContentLoaded', e => {
-    const validator = new VanillaValidator()
-    const rules = validator.setRules({
-        '[name="name"]':'required|min:2|max:5',
-        '[name="password"]': 'min:6'
-    })  
+
+const validator = new VanillaValidator()
+const form = document.getElementById('form')
+
+validator.setFields({
+    '[name="name"]':'required|min:5',
+    '[name="password"]': 'min:6'
+})
+validator.setMessages({
+    '[name="name"].required': 'O campo nome é obrigatório'
+})
+
+
+document.getElementById('form').addEventListener('submit', e => {
+    e.preventDefault();
 
     validator.validate()
-
 })
+// document.addEventListener('DOMContentLoaded', e => {
+//     // validator.validate()
+
+// })
--- validator.js
+++ validator.js
@@ -1,41 +1,123 @@
 class VanillaValidator {
+    constructor() {
+        this.fields = []
+        this.messages = []
+        this.errors = []
+
+        // this.validate().bind(this.errors)
+    }
+
     /**
      * Key: input to use in query selector
      * 
      * Value: rules to validate
-     * @param {Object} rules 
+     * @param {Object} rules Regra para validação, usando um object {'input': 'regras|separados_por_pipe}
      */
-    setRules(rules) {
-        rules = Object.values(rules).map(rule => {
-            return rule.split('|').map(rule => {
-                let position = rule.indexOf(':')
-                let values = (() => {
-                    return position !== -1 ? rule.substring((position) + 1) : ""
-                })()
-                let name = (() => {
-                    return position !== -1 ? rule.substring(0, position).trim() : rule.trim()
-                })()
+    setFields(validationOptions) {
+        let validator
+        let rules
+        let rule
+        let ruleName
+        let options = []
 
-                return { name, values }
-            })
-        })
-        this.inputs = Object.keys(rules)
-        this.rules = rules
+        for (let validationField in validationOptions) {
+            validator = {}
+            validator.validationField = validationField.trim()
+            validator.rules = {}
+    
+            rules = validationOptions[validationField].split('|')
+            rules = rules.map(rule => rule.trim())
+
+            for (let ruleIndex in rules) {
+                rule = rules[ruleIndex]
+                options = []
+
+                if (rule.indexOf(':') !== -1) {
+                    ruleName = rule.substring(0, rule.indexOf(':'))
+                    options.push(rule.substring(rule.indexOf(':') + 1).split(','))
+                } else {
+                    ruleName = rule
+                }
+
+                validator.rules[ruleName] = {
+                    options: options
+                }
+            }
+
+            this.fields.push(validator)
+        }
+    }
+
+    /**
+     * 
+     * @param {Object} messages Mensagens da validações, usando um object {'input.regra': 'mensagem'}
+     */
+    setMessages(messages) {
+        this.messages = messages
+    }
+
+    getMessage(input, rule, option = null) {
+
+    }
+
+    rules(field, ruleName, args) {
+        const validationField = document.querySelector(field)
+        const rules = {
+            required() {
+                if (validationField.value == "") {
+                    return `O campo ${field} não pode ser vazio`
+                }
+            },
+            min() {
+                if (validationField.value.length < args.options[0]) {
+                    return `O campo ${field} deve ser maior que ${args.options[0]}`
+                }
+            },
+            max() {
+                if (validationField.value.length > args.options[0]) {
+                    return `O campo ${field} deve ser menor que ${args.options[0]}`
+                }
+            },
+        }
+        
+        if (validationField == null) {
+            return console.error(`The field '${field} does not exist in your form'`)
+        }
+
+        return rules[ruleName]()
     }
 
     validate() {
-        if (typeof this.inputs == 'undefined') {
-            console.error('Must define inputs using VanillaValidator.setRules')
+        let message
+
+        if (this.allRequiredParametersAreDefined()) {
+            this.errors = []
+            for (let field of this.fields) {
+                for (let rule in field.rules) {
+                    message = this.rules(field.validationField, rule, field.rules[rule])
+                    
+                    if (typeof message != 'undefined') {
+                        this.errors.push(message)
+                    }
+                }
+            }
         }
-        if (typeof this.rules == 'undefined') {
-            console.error('Must define rules using VanillaValidator.setRules')
-        }
+
+        console.log(this.errors)
     }
 
-    // min (input, min, message = null) {
-    //     let input = document.querySelector(input)
-    //     if (input.value.length < min) {
-    //         return `${input.getAttribute('van-alias') ?? input.getAttribute('name')} deve ser maior que ${min}`
-    //     }
-    // }
+    allRequiredParametersAreDefined() {
+        if (typeof this.fields == 'undefined') {
+            console.error('Must define fields using VanillaValidator.setFields')
+
+            return false
+        }
+        if (typeof this.messages == 'undefined') {
+            console.error('Must define messages using VanillaValidator.setMessages')
+
+            return false
+        }
+
+        return true
+    }
 }
